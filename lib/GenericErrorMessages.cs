e977c990191763edf567d036b9123e48865fbac1
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2018-07-11 11:00:38 +0300

    Add another generic error message
    This time for unsupported wanted output type

--- lib/GenericErrorMessages.cs
+++ lib/GenericErrorMessages.cs
@@ -7,6 +7,11 @@ namespace DatagenSharp
 		public static string UnsupportedParameterType(string callerName, string where, Type unsupportedType)
 		{
 			return $"{callerName} does not support {unsupportedType} as {where} paramater type!";
-		} 
+		}
+
+		public static string UnsupportedWantedOutputType(string callerName, Type unsupportedOutputType)
+		{
+			return $"{callerName} does not support {unsupportedOutputType} as output type!";
+		}
 	}
 }
