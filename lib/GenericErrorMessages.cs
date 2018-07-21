67903af343049446e02f5294eda1cd9d657f43c2
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2018-07-21 20:11:49 +0300

    Add new error message
    And also fix typo in old one

--- lib/GenericErrorMessages.cs
+++ lib/GenericErrorMessages.cs
@@ -6,12 +6,17 @@ namespace DatagenSharp
 	{
 		public static string UnsupportedParameterType(string callerName, string where, Type unsupportedType)
 		{
-			return $"{callerName} does not support {unsupportedType} as {where} paramater type!";
+			return $"{callerName} does not support {unsupportedType} as {where} parameter type!";
 		}
 
 		public static string UnsupportedWantedOutputType(string callerName, Type unsupportedOutputType)
 		{
 			return $"{callerName} does not support {unsupportedOutputType} as output type!";
+		}
+
+		public static string UnsupportedInputType(string callerName, Type unsupportedInputType)
+		{
+			return $"{callerName} does not support {unsupportedInputType} as input type!";
 		}
 	}
 }
