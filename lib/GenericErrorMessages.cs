ab4676ee96f88f0180d363554546957bde56f3c4
Kaarlo Räihä <kaarlo.raiha@gmail.com>
2018-07-25 21:59:39 +0300

    Add UnsupportedParameterValue error message
    Since it will be needed in multiple places

--- lib/GenericErrorMessages.cs
+++ lib/GenericErrorMessages.cs
@@ -9,6 +9,11 @@ namespace DatagenSharp
 			return $"{callerName} does not support {unsupportedType} as {where} parameter type!";
 		}
 
+		public static string UnsupportedParameterValue(string callerName, string where, string unsupportedValue)
+		{
+			return $"{callerName} does not support {unsupportedValue} as {where} parameter value!";
+		}
+
 		public static string UnsupportedWantedOutputType(string callerName, Type unsupportedOutputType)
 		{
 			return $"{callerName} does not support {unsupportedOutputType} as output type!";
