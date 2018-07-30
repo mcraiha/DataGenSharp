9b846b937df3f6fffb14c354b2e894b4a21a4b91
Kaarlo Räihä <kaarlo.raiha@gmail.com>
2018-07-30 22:05:23 +0300

    Add GuidGenerator
    Also include test cases for it

---
+++ lib/GuidGenerator.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace DatagenSharp
+{
+	public class GuidGenerator : IDataGenerator
+	{
+		public static readonly string LongName = "GuidGenerator";
+		
+		public static readonly string Description = "Generate GUID values (e.g. 123e4567-e89b-12d3-a456-426655440000)";
+
+		public static readonly string VersionNumber = "0.9";
+
+		private static readonly Type[] supportedOutputTypes = new Type[] { typeof(string) };
+
+		private static readonly char groupSeparator = '-';
+
+		private Random rng = null;
+
+		private byte[] currentValue = new byte[16];
+
+		public (bool success, string possibleError) Init(object parameter, int seed)
+		{
+			this.rng = new Random(seed);
+
+			// Generate first value
+			this.NextStep();
+
+			return (success: true, possibleError: "");
+		}
+
+		public (bool success, string possibleError, object result) Generate(object parameter = null, Type wantedOutput = null)
+		{
+			if (wantedOutput == null)
+			{
+
+			}
+			else if (Array.IndexOf(supportedOutputTypes, wantedOutput) > -1)
+			{
+				
+			}
+			else
+			{
+				return (success: false, possibleError: ErrorMessages.UnsupportedWantedOutputType(LongName, wantedOutput), result: null);
+			}
+
+			object returnValue = FormatGuid(this.currentValue);
+
+			return (success: true, possibleError: "", result: returnValue);
+		}
+
+		private static string FormatGuid(byte[] bytes)
+		{
+			StringBuilder hex = new StringBuilder(bytes.Length * 2);
+
+			int index = 0;
+			foreach (byte b in bytes)
+			{
+				hex.AppendFormat("{0:x2}", b);
+				index++;
+				if (index == 4 || index == 6 || index == 8 || index == 10)
+				{
+					hex.Append(groupSeparator);
+				}
+			}
+    
+  			return hex.ToString();
+		}
+
+		public Type[] GetSupportedOutputs()
+		{
+			return supportedOutputTypes;
+		}
+
+		public void NextStep()
+		{
+			this.rng.NextBytes(currentValue);
+		}
+	}
+}
---
+++ tests/GuidGeneratorTests.cs
@@ -0,0 +1,98 @@
+using NUnit.Framework;
+using DatagenSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests
+{
+	public class GuidGeneratorTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void InitTest()
+		{
+			// Arrange
+			object valid1 = null;
+			int seed = 1337;
+
+			GuidGenerator gg1 = new GuidGenerator();
+
+			// Act
+			var shouldBeValidResult1 = gg1.Init(valid1, seed);
+
+			// Assert
+			Assert.IsTrue(shouldBeValidResult1.success, "Init should have been successful");
+			Assert.IsTrue(string.IsNullOrEmpty(shouldBeValidResult1.possibleError));
+		}
+
+		[Test]
+		public void GenerateTest()
+		{
+			// Arrange
+			object parameter = null;
+
+			int seed = 1337;
+
+			int loopCount = 100;
+
+			GuidGenerator gg = new GuidGenerator();
+
+			List<string> guids = new List<string>();
+			List<bool> successList = new List<bool>();
+
+			// Act
+			var shouldBeValidResult = gg.Init(parameter, seed);
+			var generated1 = gg.Generate(parameter);
+			var generated2 = gg.Generate(parameter); // Same step should provide same result
+			
+			for (int i = 0; i < loopCount; i++)
+			{
+				gg.NextStep();
+				var generateResult = gg.Generate(parameter: null, wantedOutput: null);
+				successList.Add(generateResult.success);
+				guids.Add((string)generateResult.result);
+			}
+
+			// Assert
+			Assert.IsTrue(shouldBeValidResult.success, "Init should have been successful");
+
+			Assert.AreEqual(generated1.result, generated2.result, "Both generates should have same results since NextStep has not been called between them");
+			
+			CollectionAssert.DoesNotContain(successList, false, "All generates should have been successful");
+			CollectionAssert.AllItemsAreUnique(guids, "In theory every GUID should be unique");
+		}
+
+		[Test]
+		public void CheckThatCanBeParsedTest()
+		{
+			// Arrange
+			object parameter = null;
+
+			int seed = 1337;
+
+			int loopCount = 100;
+
+			GuidGenerator gg = new GuidGenerator();
+
+			List<bool> parseWasOk = new List<bool>();
+
+			// Act
+			var shouldBeValidResult = gg.Init(parameter, seed);
+			for (int i = 0; i < loopCount; i++)
+			{
+				var generateResult = gg.Generate(parameter: null, wantedOutput: null);
+				gg.NextStep();
+				parseWasOk.Add(Guid.TryParse((string)generateResult.result, out _));
+			}
+
+			// Assert
+			Assert.IsTrue(shouldBeValidResult.success, "Init should have been successful");
+			Assert.AreEqual(parseWasOk.Count, parseWasOk.Where(item => item == true).Count(), "All of the results should be true");
+		}
+	}
+}
