891b929f49678ff63d406db8843a5e5c88ceea3e
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2018-08-18 22:00:32 +0300

    Make TSV output work
    Also add 2 test cases for SomeSeparatedValueOutput

--- lib/SomeSeparatedValueOutput.cs
+++ lib/SomeSeparatedValueOutput.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Text;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace DatagenSharp
@@ -11,12 +12,12 @@ namespace DatagenSharp
 		
 		public static readonly string Description = "Use this output if you want text where entries are separated by chars. e.g. comma-separated values (CSV) or tab-separated values (TSV)";
 
-		public static readonly string VersionNumber = "0.9";
+		public static readonly string VersionNumber = "0.9.1";
 
 		private static readonly List<(string presetName, string separator, string[] dangerousEntries)> predefinedPresets = new List<(string, string, string[])>()
 		{
-			("csv", 	",", 	new[] {",", "\""}),
-			("tsv", 	"\t", 	new[] {"\t"}),
+			("csv", 	",", 	new[] {",", "\"", "\r", "\n"}),
+			("tsv", 	"\t", 	new[] {"\t", "\r", "\n"}),
 		};
 
 		private static readonly List<Type> supportedParameterTypes = new List<Type>()
@@ -30,14 +31,29 @@ namespace DatagenSharp
 			("tsv", 	0, 	"Set output to TSV (tab-separated values), see https://en.wikipedia.org/wiki/Tab-separated_values"),
 		};
 
+		/// <summary>
+		/// Chosen preset
+		/// </summary>
 		private (string presetName, string separator, string[] dangerousEntries) preset;
 
+		/// <summary>
+		/// Output for writing data
+		/// </summary>
 		private StreamWriter output;
 
+		/// <summary>
+		/// Is header line wanted
+		/// </summary>
 		private bool headerIsWanted = true;
 
+		/// <summary>
+		/// Should last line be new line
+		/// </summary>
 		private bool lastLineShouldHaveNewLine = true;
 
+		/// <summary>
+		/// By default we don't want to write BOM https://en.wikipedia.org/wiki/Byte_order_mark
+		/// </summary>
 		private bool writeBom = false;
 
 		public (bool success, string possibleError) Init(object parameter, Stream outputStream)
@@ -48,17 +64,55 @@ namespace DatagenSharp
 				string error = ErrorMessages.UnsupportedParameterType(LongName, "Init", parameter.GetType());
 				return (success: false, possibleError: error);
 			}
+			else if (parameter == null)
+			{
+				// If no parameters are given then CSV is written
+				this.preset = predefinedPresets[0];
+				this.writeBom = false;
+			}
+			else
+			{
+				string stringParameter = (string)parameter;
+				Dictionary<string, object> paramDict = ParameterParser.GetParameterDictionary(stringParameter);
 
-			if (writeBom)
+				// First preset
+				string[] presetNames = SomeSeparatedValueOutput.predefinedPresets.Select(x => x.presetName).ToArray();
+				foreach (string presetName in presetNames)
+				{
+					if (paramDict.ContainsKey(presetName))
+					{
+						this.preset = SomeSeparatedValueOutput.predefinedPresets.Find(x => x.presetName == presetName);
+						break;
+					}
+				}
+
+				// Then is header wanted
+				if (paramDict.ContainsKey(nameof(headerIsWanted)))
+				{
+					this.headerIsWanted = (bool)paramDict[nameof(headerIsWanted)];
+				}
+
+				// Then if last line should have new line
+				if (paramDict.ContainsKey(nameof(lastLineShouldHaveNewLine)))
+				{
+					this.lastLineShouldHaveNewLine = (bool)paramDict[nameof(lastLineShouldHaveNewLine)];
+				}
+
+				// Finally if BOM should be used
+				if (paramDict.ContainsKey(nameof(writeBom)))
+				{
+					this.writeBom = (bool)paramDict[nameof(writeBom)];
+				}
+			}
+
+			if (this.writeBom)
 			{
 				this.output = new StreamWriter(outputStream, Encoding.UTF8);
 			}
 			else
 			{
 				this.output = new StreamWriter(outputStream, new UTF8Encoding(false));
-			}
-
-			this.preset = predefinedPresets[0];
+			}	
 
 			return (success: true, possibleError: "");
 		}
---
+++ tests/SomeSeparatedValueOutputTests.cs
@@ -0,0 +1,87 @@
+using NUnit.Framework;
+using DatagenSharp;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+	public class SomeSeparatedValueOutputTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void CsvSmokeTest()
+		{
+			// Arrange
+			SomeSeparatedValueOutput csvOutput = new SomeSeparatedValueOutput();
+			MemoryStream ms = new MemoryStream();
+			string parameter = "csv";
+			List<object> headers = new List<object> {"one", "two", "three"};
+			List<object> values = new List<object> {1, 2, 3};
+			string expectedOutput = "one,two,three\r\n1,2,3\r\n";
+
+			// Act
+			(bool initSuccess, string possibleInitError) = csvOutput.Init(parameter, ms);
+			(bool writeHeaderSuccess, string possibleWriteHeaderError) = csvOutput.WriteHeader(headers);
+			(bool writeSingleSuccess, string possibleWriteSingleError) = csvOutput.WriteSingleEntry(values);
+			(bool writeFooterSuccess, string possibleWriteFooterError) = csvOutput.WriteFooter(null);
+
+			string result = Encoding.UTF8.GetString(ms.ToArray());
+
+			// Assert
+			Assert.IsTrue(initSuccess, "Init should have succeeded");
+			Assert.IsTrue(string.IsNullOrEmpty(possibleInitError), "There should not be init error");
+
+			Assert.IsTrue(writeHeaderSuccess, "Header write should have succeeded");
+			Assert.IsTrue(string.IsNullOrEmpty(possibleWriteHeaderError), "There should not be write header error");
+
+			Assert.IsTrue(writeSingleSuccess, "Single entry write should have succeeded");
+			Assert.IsTrue(string.IsNullOrEmpty(possibleWriteSingleError), "There should not be single entry write error");
+
+			Assert.IsTrue(writeFooterSuccess, "Footer write should have succeeded");
+			Assert.IsTrue(string.IsNullOrEmpty(possibleWriteFooterError), "There should not be write footer error");
+
+			Assert.AreEqual(expectedOutput, result);
+		}
+
+		[Test]
+		public void TsvSmokeTest()
+		{
+			// Arrange
+			SomeSeparatedValueOutput tsvOutput = new SomeSeparatedValueOutput();
+			MemoryStream ms = new MemoryStream();
+			string parameter = "tsv";
+			List<object> headers = new List<object> {"one", "two", "three"};
+			List<object> values = new List<object> {1, 2, 3};
+			string expectedOutput = "one\ttwo\tthree\r\n1\t2\t3\r\n";
+
+			// Act
+			(bool initSuccess, string possibleInitError) = tsvOutput.Init(parameter, ms);
+			(bool writeHeaderSuccess, string possibleWriteHeaderError) = tsvOutput.WriteHeader(headers);
+			(bool writeSingleSuccess, string possibleWriteSingleError) = tsvOutput.WriteSingleEntry(values);
+			(bool writeFooterSuccess, string possibleWriteFooterError) = tsvOutput.WriteFooter(null);
+
+			string result = Encoding.UTF8.GetString(ms.ToArray());
+
+			// Assert
+			Assert.IsTrue(initSuccess, "Init should have succeeded");
+			Assert.IsTrue(string.IsNullOrEmpty(possibleInitError), "There should not be init error");
+
+			Assert.IsTrue(writeHeaderSuccess, "Header write should have succeeded");
+			Assert.IsTrue(string.IsNullOrEmpty(possibleWriteHeaderError), "There should not be write header error");
+
+			Assert.IsTrue(writeSingleSuccess, "Single entry write should have succeeded");
+			Assert.IsTrue(string.IsNullOrEmpty(possibleWriteSingleError), "There should not be single entry write error");
+
+			Assert.IsTrue(writeFooterSuccess, "Footer write should have succeeded");
+			Assert.IsTrue(string.IsNullOrEmpty(possibleWriteFooterError), "There should not be write footer error");
+
+			Assert.AreEqual(expectedOutput, result);
+		}
+	}
+}
