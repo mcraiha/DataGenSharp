3de87038670f9248fd10db59052c48fd45291071
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-04-05 21:28:02 +0300

    Add ISerialization interface
    Since there will be need to (de)serialize so many things

---
+++ lib/ISerialization.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace DatagenSharp
+{
+	/// <summary>
+	/// Interface for every thing that needs (de)serialization
+	/// </summary>
+	public interface ISerialization
+	{
+		/// <summary>
+		/// Load / deserialize from string
+		/// </summary>
+		/// <param name="parameter">String that will be loaded / deserialized</param>
+		/// <returns>Valuetuple that tells if load / deserilization was success, and possible error message</returns>
+		(bool success, string possibleError) Load(string parameter);
+
+		/// <summary>
+		/// Save / serialize to string
+		/// </summary>
+		/// <returns>String that contains serialization</returns>
+		string Save();
+	}
+}
