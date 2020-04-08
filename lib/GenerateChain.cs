cbe3019c8fd7ab6fc62fc0ced73144c84ab25ea7
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-04-08 21:42:56 +0300

    Add MutatorChains to GenerateChain
    And also add some Save() code

--- lib/GenerateChain.cs
+++ lib/GenerateChain.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace DatagenSharp
 {
 	public class GenerateChain
 	{
 		public List<IDataGenerator> DataGenerators = new List<IDataGenerator>();
+
+		public List<MutatorChain> MutatorChains = new List<MutatorChain>();
 
 		public List<(string name, IDataGenerator generator, object parameter, Type wantedOutput, MutatorChain mutatorChain)> WantedElements = new List<(string, IDataGenerator,  object, Type, MutatorChain)>();
 
@@ -54,7 +57,37 @@ namespace DatagenSharp
 
 		public string Save()
 		{
-			return "";
+			StringBuilder sb = new StringBuilder();
+
+			// Data generators first
+			sb.AppendLine(CommonSerialization.generators);
+			int currentIndex = 1;
+			foreach (IDataGenerator dataGenerator in this.DataGenerators)
+			{
+				if (dataGenerator is ISerialization serialization)
+				{
+					sb.AppendLine($"{currentIndex} {dataGenerator.GetNames().shortName}{serialization.Save()}");
+					currentIndex++;
+				}
+			}
+
+			// Then mutator chains
+			sb.AppendLine(CommonSerialization.mutatorChains);
+			currentIndex = 1;
+			foreach (MutatorChain mutatorChain in this.MutatorChains)
+			{
+				sb.AppendLine($"{currentIndex} {mutatorChain.Save()}");
+				currentIndex++;
+			}
+
+			// Finally Wanted outputs
+			sb.AppendLine(CommonSerialization.wantedElements);
+			foreach ((string name, IDataGenerator generator, object parameter, Type wantedOutput, MutatorChain mutatorChain) in this.WantedElements)
+			{
+				sb.AppendLine($"{CommonSerialization.itemStartChar}{name}{CommonSerialization.delimiter}{1}");
+			}
+
+			return sb.ToString();
 		}
 
 		#endregion // Serialization
